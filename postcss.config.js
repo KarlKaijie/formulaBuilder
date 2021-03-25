1f5f74b4cfe6ef3be18364d2c18d9407d39e2d0a
Karl <zhangkaijie163@gmail.com>
2021-03-25 10:41:16 +0800

    initialize

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,5 @@
-/*
- * @Author: Karl | ZhangKaiJie
- * @Date: 2019-01-13 18:37:30
- * @Last Modified by: Karl
- * @Last Modified time: 2019-01-14 15:53:55
- */
-/* eslint-disable global-require */
 module.exports = {
   plugins: [
     require('autoprefixer'),
   ],
 };
-/* eslint-enable global-require */
